96e494faac6a2d691ade8890767472672ab3ce44
Dmr.Erdinc <Dmr.Erdinc@DMR-ERDINC>
2024-10-02 21:33:32 +0300

    Proje dosyası ekle.

---
+++ C#/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace C_
+{
+    partial class Form
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeniSekmeAçToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.geçmişToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programdanÇıkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.düzenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ileriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.geriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.düzenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniSekmeAçToolStripMenuItem,
+            this.geçmişToolStripMenuItem,
+            this.programdanÇıkToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // yeniSekmeAçToolStripMenuItem
+            // 
+            this.yeniSekmeAçToolStripMenuItem.Name = "yeniSekmeAçToolStripMenuItem";
+            this.yeniSekmeAçToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.yeniSekmeAçToolStripMenuItem.Text = "Yeni sekme Aç";
+            // 
+            // geçmişToolStripMenuItem
+            // 
+            this.geçmişToolStripMenuItem.Name = "geçmişToolStripMenuItem";
+            this.geçmişToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.geçmişToolStripMenuItem.Text = "Geçmiş";
+            // 
+            // programdanÇıkToolStripMenuItem
+            // 
+            this.programdanÇıkToolStripMenuItem.Name = "programdanÇıkToolStripMenuItem";
+            this.programdanÇıkToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.programdanÇıkToolStripMenuItem.Text = "Programdan Çık";
+            // 
+            // düzenToolStripMenuItem
+            // 
+            this.düzenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ileriToolStripMenuItem,
+            this.geriToolStripMenuItem,
+            this.homeToolStripMenuItem});
+            this.düzenToolStripMenuItem.Name = "düzenToolStripMenuItem";
+            this.düzenToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.düzenToolStripMenuItem.Text = "Düzen";
+            // 
+            // ileriToolStripMenuItem
+            // 
+            this.ileriToolStripMenuItem.Name = "ileriToolStripMenuItem";
+            this.ileriToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.ileriToolStripMenuItem.Text = "İleri";
+            // 
+            // geriToolStripMenuItem
+            // 
+            this.geriToolStripMenuItem.Name = "geriToolStripMenuItem";
+            this.geriToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.geriToolStripMenuItem.Text = "Geri";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.homeToolStripMenuItem.Text = "Home";
+            // 
+            // panel1
+            // 
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.panel1.Location = new System.Drawing.Point(0, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 32);
+            this.panel1.TabIndex = 4;
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(538, 65);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser2.TabIndex = 6;
+            this.webBrowser2.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser2_DocumentCompleted);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimizeBox = false;
+            this.Name = "Form";
+            this.Text = "FrmGiris";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yeniSekmeAçToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem geçmişToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programdanÇıkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem düzenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ileriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem geriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+    }
+}
+
---
+++ C#/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hosgeldin....");
+                
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser2.Navigate("https://www.youtube.com/");
+        }
+
+        private void webBrowser2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
